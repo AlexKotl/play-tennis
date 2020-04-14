0726dbc2855b4894bc45a16bf28b965054095747
Alex <slicer256@gmail.com>
2020-04-15 01:31:07 +0300

    better main layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,116 @@
-<!doctype html>
+<!DOCTYPE HTML>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<head xmlns:fb="http://ogp.me/ns/fb#">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <meta name="Language" content="ru"/>
+    <meta name="country" content="UA"/>
+    <meta property="og:image" content="/images/stuff/handshake.jpg" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('styles.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/theme.bootstrap_4.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
+    <link href="{{ asset('images/favicon.png') }}" rel="shortcut icon" type="image/png">
 
-    <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    {{-- <script src="/js/jquery.slim.min.js"></script>
+    <script src="/js/bootstrap.bundle.min.js"></script> --}}
+    <script src="{{ asset('js/jquery.tablesorter.min.js') }}" defer></script>
+    <script src="{{ asset('js/jquery.mask.min.js') }}" defer></script>
+    <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js" integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og==" crossorigin=""></script>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('scripts')
+
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+    <div class="top-menu">
+        <div class="top-menu-container clearfix">
+            <div class="title float-left">
+                <a href="/">
+                    <img src="/images/tennis-ball.png" alt="">
+                    <span>Play</span>
+                    <span class='highlight'>Tennis</span>
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
             </div>
-        </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+            <div class="menu user-block float-right">
+                @guest
+                    <a href="{{ route('login') }}" class="btn btn-outline-primary">
+                        <i class="fa fa-sign-in"></i>
+                        Войти
+                    </a>
+                    <a href="{{ route('register') }}" class="btn btn-primary">
+                        <i class="fa fa-user-plus"></i>
+                        Регистрация
+                    </a>
+
+                @else
+                    <span class="profile-actions">
+                        Привет,
+                        <a href='{{ route('profile') }}'>
+                            <i class="fa fa-user"></i>
+                            {{ Auth::user()->name }}
+                        </a>
+                    </span>
+                    <a href="{{ route('logout') }}" class="btn btn-outline-primary" style="margin-left: 10px">
+                        <i class="fa fa-sign-out"></i>
+                        Выйти
+                    </a>
+                @endguest
+            </div>
+
+            <div class="menu main-chapters float-right">
+                <a href="{{ route('index') }}" class="menu-item">
+                    <img src="/images/icons/racket-white.png" class="icon" alt="">
+                    Игроки
+                </a>
+                <a href="{{ route('index') }}" class="menu-item">
+                    <img src="/images/icons/court-white.png" class="icon" alt="">
+                    Корты
+                </a>
+                <a href="{{ route('index') }}" class="menu-item" style="margin-right: 30px">
+                    <img src="/images/icons/handshake-white.png" class="icon" alt="">
+                    Друзья
+                    <span class="badge badge-danger">{{ 'menu_friends_count' }}</span>
+                </a>
+            </div>
+
+        </div>
     </div>
+
+    <div class="breadcrumbs-block">
+        <nav class="breadcrumbs clearfix">
+            <a href="/" class="">Play Tennis</a>
+            @yield('breadcrumbs')
+        </nav>
+    </div>
+
+    <div class="main-container site-width">
+        @yield('content')
+    </div>
+
+    <div class="site-footer">
+        <div class="top-menu-container clearfix text-center justify-content-center align-self-center">
+            PlayTennis.com.ua - Поиск партнеров по теннису в Киеве &copy; {{ date('Y') }}
+            <br>Разработка: <a href="https://www.facebook.com/kotl.alex">Александр Котляров</a>
+        </div>
+    </div>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-9891041-13"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-9891041-13');
+    </script>
 </body>
 </html>
