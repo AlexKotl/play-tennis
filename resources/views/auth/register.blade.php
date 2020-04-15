63f2088d4959a3064515ec42b3153ba871c917e6
Alex <slicer256@gmail.com>
2020-04-15 14:58:01 +0300

    very basic registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,276 @@
 @extends('layouts.app')
 
+{{-- {% block site_title %}
+
+    {% if request.path == '/register/' %}
+        Регистрация профиля теннисиста
+    {% else %}
+        Профиль
+    {% endif %}
+{% endblock %}
+
+{% block breadcrumbs %}
+    {% if request.path == '/register/' %}
+        <a href="">Регистрация</a>
+    {% else %}
+        <a href="">Профиль игрока</a>
+    {% endif %}
+{% endblock %} --}}
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <script>
+        $(function() {
+            // init phone mask
+            $('.phone_mask').mask("(0__) ___ __ __", {
+                placeholder: "(0__) ___ __ __",
+                translation: {
+                    0: null, 9: null, '_': {pattern: /\d/, optional: false}
+                }
+            });
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+            // hide map for mobile devices
+            if ($(window).width() <= 576) {
+                $('#collapse_map').removeClass('show');
+            }
+        })
+    </script>
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+    {{-- {% if request.path == '/register/' %}
+        <h1>Регистрация</h1>
+    {% else %}
+        <h1>Ваш профиль</h1>
+    {% endif %} --}}
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+    {{-- {% if form.errors %}
+        {% for field in form %}
+            {% for error in field.errors %}
+                <div class="alert alert-danger">
+                    <strong>{{ error|escape }}</strong>
+                </div>
+            {% endfor %}
+        {% endfor %}
+        {% for error in form.non_field_errors %}
+            <div class="alert alert-danger">
+                Not field error: <strong>{{ error|escape }}</strong>
+            </div>
+        {% endfor %}
+    {% endif %}
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+    {% if messages %}
+        <div class="alert alert-success">
+        {% for message in messages %}
+            {{ message }}
+        {% endfor %}
+        </div>
+    {% endif %} --}}
+
+    <form method="post" enctype="multipart/form-data">
+        @csrf
+
+        <div class="form-group row">
+            <label class="col-md-3 col-form-label">Имя</label>
+            <div class="col-md-9">
+                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                <small class="form-text text-muted">Ваше полное имя, которое будет отображаться на сайте</small>
+                @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label class="col-md-3 col-form-label">Пол</label>
+            <div class="col-md-9">
+                <input type="checkbox" name="" value="">
+            </div>
+        </div>
+
+        {{-- <div class="form-group row">
+            <label class="col-md-3 col-form-label">Телефон</label>
+            <div class="col-md-9">
+                {{ form.phone.errors }}
+                <input type="text" name="phone" value="{% if form.phone.value != None %}{{ form.phone.value }}{% endif %}" maxlength="180" class="form-control phone_mask">
+                <small class="form-text text-muted">Телефон будет отображаться только для ваших друзей</small>
+            </div>
+        </div>
+
+        {% if request.path == '/register/' %}
+        <div class="form-group row">
+            <label class="col-md-3 col-form-label">Email</label>
+            <div class="col-md-9">
+                {{ form.email.errors }}
+                {{ form.email }}
+            </div>
+        </div>
+        {% endif %}
+
+        <div class="form-group row">
+            <label class="col-md-3 col-form-label">Корты</label>
+            <div class="col-md-9">
+                <p>
+                    <small class="form-text text-muted">Отметьте корты на которых вы можете играть на карте или выберите из списка ниже:</small>
+                </p>
+
+                <a class="btn btn-primary d-sm-none" data-toggle="collapse" href="#collapse_map">
+                    <i class="fa fa-map"></i> Показать карту
+                </a>
+                <div id="collapse_map" class="collapse show">
+                    <div id="map" class="map" style="width:100%; height:400px"></div>
+                </div>
+
+                <script type="text/javascript">
+                    var map = L.map('map').setView([50.4425, 30.5133], 12);
+
+                    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                        attribution:'Map data <a target="_blank" href="http://www.openstreetmap.org">OpenStreetMap.org</a> contributors, ' +
+                            '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+                        maxZoom: 18,
+                    }).addTo(map);
+
+                    var MarkerIcon = L.Icon.extend({
+                        options: {
+                            iconSize: [25, 41],
+                            iconAnchor: [13, 41],
+                            popupAnchor: [0, -41]
+                        }
+                    });
+                    var IconSelected = new MarkerIcon({iconUrl: '{% static 'images/icons/map-marker-ball.png' %}'});
+                    var Icon = new MarkerIcon({iconUrl: '{% static 'images/icons/map-marker.png' %}'});
+                    var markers = [];
+
+                    {% for court in form.fields.courts.queryset %}
+                        {% if court.map_lat != 0 and court.map_lng != 0 %}
+                            var marker = L.marker([{{ court.map_lat }}, {{ court.map_lng }}], {
+                                icon: {% if court.id in form.courts.value %} IconSelected {% else %} Icon {% endif %}
+                            }).addTo(map);
+                            marker.bindPopup("<b>{{ court.name }}</b>");
+                            marker.isSelected = false;
+                            marker.courtId = {{ court.id }};
+                            marker.on('click', function(element) {
+                                var is_selected = element.target.isSelected;
+                                element.target.isSelected = !is_selected;
+                                element.target.setIcon(is_selected ? Icon : IconSelected);
+                                $('input[name=courts][value={{ court.id }}]').prop('checked', !is_selected);
+                            });
+
+                            markers['{{ court.id }}'] = marker;
+                        {% endif %}
+                    {% endfor %}
+
+                    $(document).ready(function() {
+                        $('input[name=courts]').on('change', function(element) {
+                            if (markers[$(this).val()] !== undefined) {
+                                markers[$(this).val()].fire('click');
+                            }
+                        });
+                    })
+
+                </script>
+
+                <div class="row" style="padding-left:15px; padding-top: 15px">
+                    {{ form.courts.errors }}
+                    {% for court in form.fields.courts.queryset %}
+                        <div class="form-check col-12 col-sm-4">
+                            <label class="form-check-label">
+                                <input class="form-check-input" name="courts" type="checkbox" value="{{ court.id }}" {% if court.id in form.courts.value %} checked {% endif %} />
+                                {{ court.name }}
+                            </label>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    {% endfor %}
                 </div>
             </div>
         </div>
-    </div>
-</div>
+
+        <div class="form-group row">
+            <label class="col-md-3 col-form-label">Уровень игры</label>
+            <div class="col-md-9">
+                {{ form.rank.errors }}
+                {{ form.rank }}
+                <small class="form-text text-muted"><a href="#" data-toggle="modal" data-target="#rankModal">Как определить свой уровень?</a></small>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label class="col-md-3 col-form-label">Игровой опыт</label>
+            <div class="col-md-9">
+                {{ form.player_since.errors }}
+                {{ form.player_since }}
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label class="col-md-3 col-form-label" for="id_is_looking">&nbsp;</label>
+            <div class="col-md-9">
+                <div class="form-check">
+                    <input type="checkbox" name="is_looking" class="form-check-input" id="id_is_looking" {% if form.is_looking.value == 1 %} checked {% endif %}>
+                    <label class="form-check-label" for="id_is_looking" title="Отметьте галочку, если вы ищете партнера.">Ищу партнера</label>
+                    <small class="form-text text-muted">Отметьте галочку, если вы ищете партнера.</small>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label class="col-md-3 col-form-label">О себе</label>
+            <div class="col-md-9">
+                {{ form.about.errors }}
+                <textarea name="about" cols="10" rows="4" class="form-control" placeholder="О себе" title="Расскажите о себе, ваш опыт игры, увлечения и т.д." id="id_about">{% if form.about.value != None %}{{ form.about.value }}{% endif %}</textarea>
+                <small class="form-text text-muted">Расскажите о себе, ваш опыт игры, увлечения и т.д.</small>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label class="col-md-3 col-form-label">Фото</label>
+            <div class="col-md-9">
+                {{ form.image.errors }}
+
+                {% if form.image.value != null and form.image.value != '' %}
+                    <p>
+                        <img src="/media/{{ form.image.value }}" width="200" alt="User photo">
+                        &nbsp;
+                        <label>
+                            <input type="checkbox" name="image-clear" id="image-clear_id">
+                            Удалить фото
+                            <i class="fa fa-trash"></i>
+                        </label>
+                    </p>
+                {% endif %}
+
+
+                <input type="file" name="image" accept="image/*">
+            </div>
+        </div>
+
+        {% if request.path == '/register/' %}
+        <div class="form-group row">
+            <label class="col-md-3 col-form-label" for="id_password1">Пароль</label>
+            <div class="col-md-9">
+                <input type="password" name="password1" class="form-control" placeholder="Пароль" required="" id="id_password1">
+                <small class="form-text text-muted">
+                    <ul>
+                        <li>Должен содержать как минимум 8 символов.</li>
+                        <li>Должен содержать буквы и числа.</li>
+                    </ul>
+                </small>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label class="col-md-3 col-form-label" for="id_password2">Пароль еще раз</label>
+            <div class="col-md-9">
+                <input type="password" name="password2" class="form-control" placeholder="Пароль" required="" id="id_password2">
+                <small class="form-text text-muted">Введите такой же пароль, как вы ввели выше.</small>
+            </div>
+        </div>
+        {% endif %} --}}
+
+
+
+        <input type="submit" value="Сохранить" class="btn btn-primary">
+    </form>
+
+    @include('inc.ranks_modal')
+
+
 @endsection
