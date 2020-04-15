a78a50d13d99f6b7c7ddd44c67d48696b869dad8
Alex <slicer256@gmail.com>
2020-04-15 10:46:47 +0300

    added courts migration

---
+++ database/migrations/2020_04_15_073629_create_courts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCourtsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('url');
+            $table->float('map_lat');
+            $table->float('map_lng');
+            $table->integer('flag');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courts');
+    }
+}
