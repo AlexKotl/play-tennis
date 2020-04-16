96cfe6a31ec1afdcd8e2a7477b468c3611ddeb1c
Alex <slicer256@gmail.com>
2020-04-16 18:30:28 +0300

    mark messages as read

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -21,13 +21,23 @@ class PlayerController extends Controller
     {
         $user = User::find($id);
         $user->nickname = explode('@', $user->email)[0];
-        $auth_user_id = Auth::User()->id;
-        $messages = DB::table('messages')
-            ->select('users.name as author_name', 'messages.*')
-            ->leftJoin('users', 'users.id', '=', 'messages.author_id')
-            ->whereIn('recipient_id', [$id, $auth_user_id])
-            ->whereIn('author_id', [$id, $auth_user_id])
-            ->orderBy('messages.id', 'desc')->get();
+        $messages = [];
+        if (Auth::check()) {
+            $auth_user_id = Auth::id();
+            $messages = DB::table('messages')
+                ->select('users.name as author_name', 'messages.*')
+                ->leftJoin('users', 'users.id', '=', 'messages.author_id')
+                ->whereIn('recipient_id', [$id, $auth_user_id])
+                ->whereIn('author_id', [$id, $auth_user_id])
+                ->orderBy('messages.id', 'desc')->get();
+
+            // mark messages as read
+            if (count($messages) > 0) {
+                DB::table('messages')
+                    ->where(['author_id' => $id, 'recipient_id' => Auth::id()])
+                    ->update(['is_read' => true]);
+            }
+        }
 
         return view('player.show', [
             'player' => $user,
@@ -39,7 +49,7 @@ class PlayerController extends Controller
     public function message($id, Request $request)
     {
         $message = Message::create([
-            'author_id' => Auth::User()->id,
+            'author_id' => Auth::id(),
             'recipient_id' => $id,
             'text' => $request->input('text'),
         ]);
