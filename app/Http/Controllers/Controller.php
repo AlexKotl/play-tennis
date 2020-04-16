63b6befe22656cd801573dc8629b422a4031a4b6
Alex <slicer256@gmail.com>
2020-04-16 19:28:14 +0300

    populate friends count in main menu

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,28 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Auth;
+use View;
+use App\Message;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct()
+    {
+        $this->middleware(function ($request, $next) {
+            if (Auth::check()) {
+                $unread_messages = Message::where([
+                    'recipient_id' => Auth::id(),
+                    'is_read' => 0,
+                ])->get();
+                $friends_count = count($unread_messages);
+            }
+            View::share('menu_friends_count', $friends_count > 0 ? $friends_count : '');
+
+            return $next($request);
+        });
+
+    }
 }
