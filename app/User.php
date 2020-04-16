bbe869546b5fdf9f926e0f03f8adcab70f25dfc0
Alex <slicer256@gmail.com>
2020-04-16 21:36:49 +0300

    avatar migration

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'phone', 'rank', 'about', 'player_since'
+        'name', 'email', 'password', 'phone', 'rank', 'about', 'player_since', 'avatar_image'
     ];
 
     /**
